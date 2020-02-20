4af5b649f22ffc286ebc27debc10dc96a58dd4a7
Ignacio Paricio <ignacio.paricio@outlook.com>
2020-02-20 16:56:35 +0100

    Add functionality to send email, monitor inbox

--- email_test.py
+++ email_test.py
@@ -5,20 +5,43 @@ Created on 20/02/2020 14:52
 """
 
 import smtplib
+import imaplib
+import email
+from email.message import EmailMessage
 
-with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-    # identify ourselves, likely redundant
-    smtp.ehlo()
-    # activate encryption
-    smtp.starttls()
-    # re-identify ourselves after securing connection
-    smtp.ehlo()
+# send email
+msg = EmailMessage()
+msg['Subject'] = 'New ***REMOVED*** yoga class available'
+msg['From'] = 'sample_email@gmail.com'
+msg['To'] = ['sample_email@outlook.com', 'sample_email@gmail.com']
+msg.set_content('New options available...')
 
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login('sample_email@gmail.com', 'sample_pw')
-
-    subject = 'New session!'
-    body = 'A new class opened'
-    msg = f'Subject: {subject}\n\n{body}'
-
-    smtp.sendmail('sample_email@gmail.com', 'ignacio.outlook@outlook.com', msg)
+    smtp.send_message(msg)
     print('email sent')
+
+# monitor emails https://yuji.wordpress.com/2011/06/22/python-imaplib-imap-example-with-gmail/
+with imaplib.IMAP4_SSL('imap.gmail.com') as imap:
+    imap.login('sample_email@gmail.com', 'sample_pw')
+    imap.select("INBOX") # imap.list() to see all folders
+
+    # get all email ids; uses uid to search by unique id (they don't change if user deletes emails)
+    status, data = imap.uid('search', None, 'SUBJECT "New ***REMOVED*** yoga class available"') # can use "ALL" as second argument
+    email_uids = data[0].split()
+
+    # get latest email
+    latest_email_uid = email_uids[-1]
+    status, data = imap.uid('fetch', latest_email_uid, "(RFC822)") # fetch the email body (RFC822) for the given ID
+    raw_email = data[0][1].decode('utf-8')
+    
+    imap.close()
+
+# process email
+email_msg = email.message_from_string(raw_email)
+choice = email_msg.get_payload()
+
+
+# check messages altenative https://www.youtube.com/watch?v=njDGaVnz9Z8
+# yet another alternative https://gist.github.com/jonathanhculver/4281908
+# alternative with Google API https://developers.google.com/gmail/api/quickstart/python
